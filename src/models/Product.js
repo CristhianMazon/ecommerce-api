a22d94849006b6954822ab51e87bc48e16a9bfd7
Cristhian Mazon <cristhian.s.mazon@gmail.com>
2025-06-23 21:27:53 -0300

    Update Product.js

--- src/models/Product.js
+++ src/models/Product.js
@@ -5,26 +5,23 @@ const Category = require('./Category');
 const Product = sequelize.define('Product', {
   name: {
     type: DataTypes.STRING,
-    allowNull: false // Adicionado para garantir que o nome seja obrigatório
+    allowNull: false
   },
-  description: { // Adicionado o campo de descrição
+  description: {
     type: DataTypes.TEXT,
-    allowNull: true // Pode ser nulo se não houver descrição
+    allowNull: true
   },
   price: {
     type: DataTypes.FLOAT,
-    allowNull: false // Adicionado para garantir que o preço seja obrigatório
+    allowNull: false
   },
   stock: {
     type: DataTypes.INTEGER,
-    allowNull: false, // Adicionado para garantir que o estoque seja obrigatório
-    defaultValue: 0 // Valor padrão para estoque, se não especificado
+    allowNull: false,
+    defaultValue: 0 
   },
 });
 
-// Relacionamento Produto N:1 Categoria
-// onDelete 'SET NULL': Se uma categoria for deletada, o categoryId dos produtos associados será definido como NULL.
-// Certifique-se de que a coluna categoryId na tabela de produtos permita valores nulos no banco de dados.
 Product.belongsTo(Category, { foreignKey: 'categoryId', onDelete: 'SET NULL' });
 Category.hasMany(Product, { foreignKey: 'categoryId' });
 
